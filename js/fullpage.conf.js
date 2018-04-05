f2614b308a6969bb0b5f4ddad82c20fb87803ced
Ryan Willpower <surasak.c.work@gmail.com>
2018-04-05 17:57:18 +0700

    Delete unnessary config.

--- js/fullpage.conf.js
+++ js/fullpage.conf.js
@@ -11,39 +11,6 @@ $(document).ready(function() {
         slidesNavigation: false,
         slidesNavPosition: 'bottom',
 
-        //Scrolling
-        css3: true,
-        scrollingSpeed: 700,
-        autoScrolling: true,
-        fitToSection: true,
-        fitToSectionDelay: 1000,
-        scrollBar: false,
-        easing: 'easeInOutCubic',
-        easingcss3: 'ease',
-        loopBottom: false,
-        loopTop: false,
-        loopHorizontal: true,
-        continuousVertical: false,
-        continuousHorizontal: false,
-        scrollHorizontally: false,
-        interlockedSlides: false,
-        dragAndMove: false,
-        offsetSections: false,
-        resetSliders: false,
-        fadingEffect: false,
-        normalScrollElements: '#element1, .element2',
-        scrollOverflow: false,
-        scrollOverflowReset: false,
-        scrollOverflowOptions: null,
-        touchSensitivity: 15,
-        normalScrollElementTouchThreshold: 5,
-        bigSectionsDestination: null,
-
-        //Accessibility
-        keyboardScrolling: true,
-        animateAnchor: true,
-        recordHistory: true,
-
         //Design
         controlArrows: true,
         verticalCentered: true,
@@ -57,19 +24,6 @@ $(document).ready(function() {
         parallax: true,
         parallaxOptions: {type: 'reveal', percentage: 62, property: 'translate'},
 
-        //Custom selectors
-        sectionSelector: '.section',
-        slideSelector: '.slide',
-
         lazyLoading: true,
-
-        //events
-        onLeave: function(index, nextIndex, direction){},
-        afterLoad: function(anchorLink, index){},
-        afterRender: function(){},
-        afterResize: function(){},
-        afterResponsive: function(isResponsive){},
-        afterSlideLoad: function(anchorLink, index, slideAnchor, slideIndex){},
-        onSlideLeave: function(anchorLink, index, slideIndex, direction, nextSlideIndex){}
     });
 });
